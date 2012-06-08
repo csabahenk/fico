a510813c2ddbe914098a2e38870c3c43d436b1bb
Csaba Henk <csaba@lowlife.hu>
2012-06-08 17:35:23 +0530

    implement filtering by pattern

--- fico/fico.go
+++ fico/fico.go
@@ -10,6 +10,7 @@ import (
 	"strings"
 	"time"
 	"encoding/json"
+	"path/filepath"
 
 	"dir"
 )
@@ -28,6 +29,8 @@ const (
 var hint *int
 var debug *bool
 var workers *int
+var filter *string
+var filterfiles *bool
 
 func colorize(c int, txt string) string {
 	return fmt.Sprintf("\x1b[01;3%dm%s\x1b[00m", c, txt)
@@ -38,8 +41,23 @@ func pjoin(elem ...string) string {
 	return strings.Join(elem, "/")
 }
 
-func countdir(dpath string, c chan string) int {
-	f, err := os.Open(dpath)
+type jspec struct {
+	prefix int
+	path string
+}
+
+func (js *jspec) join(de *dir.Dirent) *jspec {
+	return &jspec{js.prefix, pjoin(js.path, de.Name)}
+}
+
+func (js *jspec) match(pat string) bool {
+	matching, _ := filepath.Match(pat, js.path[js.prefix:])
+	if *debug { fmt.Println("match", pat, js.path[js.prefix:], matching) }
+	return matching
+}
+
+func countdir(js *jspec, c chan *jspec) int {
+	f, err := os.Open(js.path)
 	if err != nil { log.Fatal(err) }
 	des, err := dir.Readdir(f, *hint)
 	f.Close()
@@ -49,68 +67,73 @@ func countdir(dpath string, c chan string) int {
 	for _, de := range des {
 		switch de.Type {
 		case dir.DT_UNKNOWN:
-			log.Fatalf("got no filetype info: %s", pjoin(dpath, de.Name))
+			log.Fatalf("got no filetype info: %s", js.join(de).path)
 		case dir.DT_REG:
+			if *filter != "" && *filterfiles && js.join(de).match(*filter) {
+				continue
+			}
 			fc += 1
 		case dir.DT_DIR:
-			c <- pjoin(dpath, de.Name)
+			jsn := js.join(de)
+			if *filter != "" && jsn.match(*filter) { continue }
+			c <- jsn
 		}
 	}
 
-	if *debug { fmt.Println(dpath, fc) }
+	if *debug { fmt.Println(js.path, fc) }
 
 	return fc
 }
 
-func counter(cwi, cwo chan string, cc chan int) {
+func counter(cwi, cwo chan *jspec, cc chan int) {
 	count := 0
 	for {
-		dp := <-cwi
-		if dp == "" {
+		js := <-cwi
+		if js == nil {
 			cc <- count
 			return
 		}
-		count += countdir(dp, cwo)
-		cwo <- ""
+		count += countdir(js, cwo)
+		cwo <- nil
 	}
 }
 
 func countdirs(dpaths []string) int {
-	cwi := make(chan string, 3 * *hint / *workers)
-	cwo := make(chan string, 3 * *hint / *workers)
+	cwi := make(chan *jspec, 3 * *hint / *workers)
+	cwo := make(chan *jspec, 3 * *hint / *workers)
 	cc := make(chan int)
 	for i := 0; i < *workers; i++ {
 		go counter(cwi, cwo, cc)
 	}
 
 	for _, dp := range dpaths {
-		cwi <- dp
+		cwi <- &jspec{len(dp) + 1, dp}
 	}
 
-	q := make([]string, 0, 3 * *hint)
+	q := make([]*jspec, 0, 3 * *hint)
 	for n := len(dpaths); n != 0; {
-		dp := <-cwo
-		if *debug { fmt.Println("jobstat", dp, n, q, len(cwi), cap(cwi)) }
-		if dp == "" {
+		js := <-cwo
+		if *debug { fmt.Println("jobstat", js, n, q, len(cwi), cap(cwi)) }
+		if js == nil {
 			n -= 1
 			if len(q) > 0 {
-				dp = q[0]
+				js = q[0]
 				q = q[1:]
 			}
 		}
-		if dp != "" {
+		if js != nil {
 			select {
-			case cwi <- dp:
+			case cwi <- js:
 				n += 1
 			default:
-				q = append(q, dp)
+				q = append(q, js)
 			}
 		}
 	}
 
 	count := 0
 	for i := 0; i < *workers; i++ {
-		cwi <- ""
+		cwi <- nil
 		count += <-cc
 	}
 
@@ -146,6 +169,8 @@ func main() {
 	debug = flag.Bool("debug", false, "debug mode")
 	hint = flag.Int("hint", 256, "filenumber hint")
 	workers = flag.Int("workers", runtime.NumCPU() + 1, "number of workers")
+	filter = flag.String("filter", "", "glob patterns to exclude")
+	filterfiles = flag.Bool("filterfiles", false, "apply filter to files, too")
 	scan := flag.Int("scanby", 10, "interval to scan by")
 	rec  := flag.Int("recby", 20, "interval to record by")
 	turns := flag.Int("turns", 0, "number of iterations")
@@ -159,6 +184,12 @@ func main() {
 		logf, err := os.OpenFile(*logp, os.O_WRONLY|os.O_CREATE, 0600)
 		if err != nil { log.Fatal(err) }
 		logj = json.NewEncoder(logf)
+	}
+
+	if *filter != "" {
+		// XXX have to match against non-empty string to test pattern
+		_, err := filepath.Match(*filter, "x")
+		if err != nil { log.Fatal(err) }
 	}
 
 	targets := flag.Args()
